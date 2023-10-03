2d252e54c4ab5df780055845d9c70ee26b57f15e
Antonio Rezende Pimenta <tonikopimenta@gmail.com>
2023-10-03 19:41:01 -0300

    Created "Users" controller, provider and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UserService } from './users/shared/user.service/user.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
