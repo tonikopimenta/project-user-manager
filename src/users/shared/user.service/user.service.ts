2fe1ea452dfc114b56d1231b82d80f7d795a9260
Antonio Rezende Pimenta <tonikopimenta@gmail.com>
2023-10-03 21:23:30 -0300

    Created crud operations without DB integration

--- src/users/shared/user.service/user.service.ts
+++ src/users/shared/user.service/user.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../user/user';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    users: User[] =[
+        {
+            id: 1, 
+            name: 'Toniko Pimenta', 
+            username: 'tonikopimenta',
+            password: 'mudar123',
+            canCreate: true,
+            canEdit: true
+        },
+        {
+            id: 2, 
+            name: 'John Cena', 
+            username: 'johncena',
+            password: 'ucantseeme',
+            canCreate: false,
+            canEdit: false
+        }
+    ]
+
+    getAll(){
+        return this.users;
+    }
+
+    getById(id: number){
+        const user = this.users.find((value) => value.id == id);
+        return user;
+    }
+
+    create(user: User){
+        let lastId = 0;
+        if (this.users.length > 0) {
+            lastId = this.users[this.users.length - 1].id;
+        }
+
+        user.id = lastId +1;
+        this.users.push(user);
+
+        return user;
+    }
+
+    update(user: User){
+        const userArray = this.getById(user.id);
+        if(userArray) {
+            userArray.name = user.name;
+            userArray.username = user.username;
+            userArray.password = user.password;
+            userArray.canCreate = user.canCreate;
+            userArray.canEdit = user.canEdit;
+        }
+
+        return userArray;
+
+    }
+
+    delete(id: number){
+        const index = this.users.findIndex((value) => value.id == id);
+        this.users.splice(index, 1);
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from './shared/user.service/user.service';
+import { User } from './shared/user/user';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(
+        private userService :UserService
+    ){ }
+
+    @Get()
+    async getAll() : Promise<User[]> {
+        return this.userService.getAll();    
+    }
+
+    @Get(':id')
+    async getById(@Param('id') id: number) : Promise<User> {
+        return this.userService.getById(id);    
+    }
+
+    @Post()
+    async create(@Body() user: User): Promise<User>{
+        return this.userService.create(user);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: number,@Body() user: User): Promise<User>{
+        user.id = id;
+        return this.userService.update(user);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: number){
+        this.userService.delete(id);
+    }
+
+
+
+}
