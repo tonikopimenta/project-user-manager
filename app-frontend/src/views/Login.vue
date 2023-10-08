4688ab63e663d3bb94f3ada4a7408e90e4748410
Antonio Rezende Pimenta <tonikopimenta@gmail.com>
2023-10-08 12:52:58 -0300

    login view created

---
+++ app-frontend/src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login">
+      <h1>Login</h1>
+      <div class="form-inputs">
+          <label for="email">Email</label>
+          <input type="text" id="email" name="email" v-model="input.email" placeholder="exempo@exemplo.com" />
+      </div>
+      <div class="form-inputs">
+          <label for="password">Senha</label>
+          <input type="password" id="password" name="password" v-model="input.password" placeholder="Senha" />
+      </div>
+      <button type="button" v-on:click="login()">Login</button>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'LoginView',
+      data() {
+          return {
+              input: {
+                  email: "",
+                  password: ""
+              }
+          }
+      },
+      methods: {}
+      }
+</script>
+
+<style>
+
+#login .form-inputs {
+  padding-bottom: 10px;
+}
+
+#login .form-inputs label {
+  padding-right: 10px;
+}
+
+</style>
